9ff79b1be7d6e9c03c098dea99b8acf3da49f537
606zn <59501607+606zn@users.noreply.github.com>
2021-08-27 01:55:30 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace Windows_Form_Spawn_and_Click_Picture_Box_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblItemCount = new System.Windows.Forms.Label();
+            this.GameTamer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblItemCount
+            // 
+            this.lblItemCount.AutoSize = true;
+            this.lblItemCount.Location = new System.Drawing.Point(28, 23);
+            this.lblItemCount.Name = "lblItemCount";
+            this.lblItemCount.Size = new System.Drawing.Size(39, 13);
+            this.lblItemCount.TabIndex = 0;
+            this.lblItemCount.Text = "Item: 0";
+            // 
+            // GameTamer
+            // 
+            this.GameTamer.Enabled = true;
+            this.GameTamer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblItemCount);
+            this.Name = "Form1";
+            this.Text = "Spawn_and_Click_Picture_Box_MOO_ICT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblItemCount;
+        private System.Windows.Forms.Timer GameTamer;
+    }
+}
+
